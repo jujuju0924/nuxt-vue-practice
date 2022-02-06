121748c7de6ba4b66f5359c93af913d49d8e3e82
jumpei arai <jumpei0924@gmail.com>
2022-02-06 14:02:53 +0900

    aaa

---
+++ components/DateListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-list-item align="start">
+    <v-list-item-content>
+      <v-list-item-title>
+        {{ date.toFormat('yyyy/MM/dd') }}
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        {{ date.toFormat('HH:mm') }} ~
+      </v-list-item-subtitle>
+    </v-list-item-content>
+
+    <v-list-item-action>
+      <v-icon color="accent">
+        mdi-delete
+      </v-icon>
+    </v-list-item-action>
+  </v-list-item>
+</template>
+
+<script>
+import { DateTime } from 'luxon'
+
+export default {
+  props: {
+    date: {
+      type: DateTime,
+      required: true,
+    },
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
---
+++ components/EventEditor.vue
@@ -0,0 +1,77 @@
+<template>
+<v-container>
+  <v-row>
+    <v-col>
+      <!-- タイトル入力 -->
+      {{ title }}
+      <v-text-field
+        label="Title"
+        required
+        counter
+        maxlength="25"
+        @change="(value) => {
+          title = value
+        }"
+      />
+      <!-- 詳細入力 -->
+      <v-textarea
+        label="Description"
+        counter
+        maxlength="200"
+        @change="(value) => {
+          description = value
+        }"
+      />
+    </v-col>
+    <v-col>
+      <v-row>
+        <v-col>
+          <!-- 時刻選択 -->
+          <v-time-picker 
+          :value="time"
+          format="24hr" 
+          @change="(value) => {
+            time = value
+          }"
+          />
+        </v-col>
+        <v-col>
+          <!-- 日付選択 -->
+          <v-date-picker
+            no-title
+            bottom
+            color="primary"
+          />
+        </v-col>
+      </v-row>
+    </v-col>
+    <v-col>
+      <!-- 日時選択解除 -->
+      <v-list
+        class="overflow-y-auto"
+        max-height="75vh"
+      >
+      <date-list-item :date="now"/>
+      </v-list>
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+
+<script>
+  import { DateTime } from 'luxon'
+
+export default{
+  data(){
+    return{
+      now: DateTime.now(),
+      title:'',
+      description:'',
+      time:'19:00',
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
